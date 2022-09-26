fbc6d66b672c3457da66d28e6dc171638b31c527
Rickard Andersson <gonz@severnatazvezda.com>
2022-09-26 23:32:54 +0300

    research: add experiment with generics & `asdf`

--- source/app.d
+++ source/app.d
@@ -1,6 +1,22 @@
 import std.stdio;
+import asdf;
+import fluent.asserts : should;
+
+struct GenericType(T)
+{
+  T value;
+}
+
+unittest
+{
+  auto s = `{"value": 42}`;
+  auto a = GenericType!int(42);
+  s.deserialize!(GenericType!(int)).should.equal(a);
+}
 
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+  auto s = `{"value": 42}`;
+  auto a = s.deserialize!(GenericType!(int));
+	writeln(a);
 }
