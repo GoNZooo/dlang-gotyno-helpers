a9c62d3d7714872a936726d30705e10682a3b983
Rickard Andersson <gonz@severnatazvezda.com>
2022-09-29 01:50:44 +0300

    research: add payload structs for constructors

--- source/genericUnion.d
+++ source/genericUnion.d
@@ -4,9 +4,27 @@ import fluent.asserts : should;
 import std.sumtype;
 import structs;
 
+struct JustHasIntData
+{
+  JustHasInt data;
+}
+
+struct GenericTypeData(T)
+{
+  GenericType!T data;
+}
+
+struct EmptyPayloadData
+{
+}
+
+struct OtherEmptyPayloadData
+{
+}
+
 struct GenericUnion(T)
 {
-  alias Type = SumType!(JustHasInt, GenericType!T, EmptyPayload);
+  alias Type = SumType!(JustHasIntData, GenericTypeData!T, EmptyPayloadData, OtherEmptyPayloadData);
   Type data;
   alias data this;
 
@@ -21,21 +39,26 @@ struct GenericUnion(T)
     final switch (tag)
     {
       case "JustHasInt": {
-        JustHasInt v = void;
-        if (auto e = asdfData["data"].deserializeValue(v)) return e;
+        JustHasIntData v = void;
+        if (auto e = asdfData.deserializeValue(v)) return e;
         data = v;
         return null;
       }
 
       case "GenericType": {
-        GenericType!T v = void;
-        if (auto e = asdfData["data"].deserializeValue(v)) return e;
+        GenericTypeData!T v = void;
+        if (auto e = asdfData.deserializeValue(v)) return e;
         data = v;
         return null;
       }
 
       case "EmptyPayload": {
-        data = EmptyPayload();
+        data = EmptyPayloadData();
+        return null;
+      }
+
+      case "OtherEmptyPayload": {
+        data = OtherEmptyPayloadData();
         return null;
       }
     }
@@ -44,20 +67,52 @@ struct GenericUnion(T)
 
 unittest
 {
-  GenericUnion!int.Type expected = GenericType!int(42);
+  GenericUnion!int.Type expected = GenericTypeData!int(GenericType!int(42));
   auto string = `{"type": "GenericType", "data": {"value": 42}}`;
   auto decoded = string.deserialize!(GenericUnion!int);
   decoded.data.should.equal(expected);
 
-  GenericUnion!int.Type expected2 = JustHasInt(1337);
+  decoded.match!(
+    (JustHasIntData _) => assert(false),
+    (GenericTypeData!int v) => assert(v.data.value == 42),
+    (EmptyPayloadData _) => assert(false),
+    (OtherEmptyPayloadData _) => assert(false)
+  );
+
+  GenericUnion!int.Type expected2 = JustHasIntData(JustHasInt(1337));
   auto string2 = `{"type": "JustHasInt", "data": {"otherValue": 1337}}`;
   auto decoded2 = string2.deserialize!(GenericUnion!int);
   decoded2.data.should.equal(expected2);
 
   decoded2.match!(
-    (JustHasInt v) => v.otherValue.should.equal(1337),
-    (GenericType!int _) => assert(false),
-    EmptyPayload => assert(false)
+    (JustHasIntData v) => v.data.otherValue.should.equal(1337),
+    (GenericTypeData!int _) => assert(false),
+    (EmptyPayloadData _) => assert(false),
+    (OtherEmptyPayloadData _) => assert(false)
+  );
+
+  GenericUnion!int.Type expected3 = EmptyPayloadData();
+  auto string3 = `{"type": "EmptyPayload"}`;
+  auto decoded3 = string3.deserialize!(GenericUnion!int);
+  decoded3.data.should.equal(expected3);
+
+  decoded3.match!(
+    (JustHasIntData _) => assert(false),
+    (GenericTypeData!int _) => assert(false),
+    (EmptyPayloadData _) => assert(true),
+    (OtherEmptyPayloadData _) => assert(false)
+  );
+
+  GenericUnion!int.Type expected4 = OtherEmptyPayloadData();
+  auto string4 = `{"type": "OtherEmptyPayload"}`;
+  auto decoded4 = string4.deserialize!(GenericUnion!int);
+  decoded4.data.should.equal(expected4);
+
+  decoded4.match!(
+    (JustHasIntData _) => assert(false),
+    (GenericTypeData!int _) => assert(false),
+    (EmptyPayloadData _) => assert(false),
+    (OtherEmptyPayloadData _) => assert(true)
   );
 }
 
